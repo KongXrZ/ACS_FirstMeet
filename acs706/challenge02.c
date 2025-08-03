083587a73f932e3bf0e9b36db35eba87e7b4a7d0
KongXrZ <kongsirawit.bs@gmail.com>
2025-08-03 23:20:46 +0700

    v6.1

--- acs706/challenge02.c
+++ acs706/challenge02.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
 int main(){
-    int i,nmenu,ncup;
-    int nmenu[3],ncup[10];
-    for (i=0;i>=0;i++){
-        scanf("%d",&nmenu[i]);
+    int i=0,count=0,menu,cup,cups=0,n=0;
+    int nmenu[10],ncup[10];
+    while(1){
+        scanf("%d",&menu);
         
-        if (nmenu!=0){
-            scanf("%d",&ncup);
-            if(ncup==0){
+        if (menu!=0&&menu<=3){
+            scanf("%d",&cup);
+            if(cup==0){
                 break;
             }
         }
         else{
             break;
         }
+        nmenu[i]=menu;
+        ncup[i]=cup;
         
-        
+        i++;
+        count++;
     }
+    float price=0,total=0,discount=0.0;
+    for(i=0;i<count;i++){
+        if(nmenu[i]==1){
+            price = ncup[i]*25;
+        }else if (nmenu[i]==2){
+            price = ncup[i]*30;
+        }else if (nmenu[i]==3){
+            price = ncup[i]*35;
+        }
+        total=total+price;
+        cups+=ncup[i];
+    }
+
+    if (total >= 300){
+        discount = total*0.05;
+    }else if (cups>=10){
+        discount = total*0.10;
+    }
+    total-=discount;
+    int intotal = (int)total;
+    if (total-intotal>=0.5)
+    {
+        intotal+=1;
+    }
+    
+    printf("%d",intotal);
     return 0;
 }
